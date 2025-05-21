b617251ee8e72eb2ba13aeb049268c2bfa5d4c9a
ARBIAARBIA <bouzidarbia99@gmail.com>
2025-05-21 23:06:59 +0100

    sans k8s

--- test/product.test.js
+++ test/product.test.js
@@ -1,43 +1,47 @@
 require('dotenv').config({ path: '.env.test' });
-
 const chai = require('chai');
 const expect = chai.expect;
 const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
 const Product = require('../product');
-
 const mongoose = require('mongoose');
-
+const jwt = require('jsonwebtoken');
 
 chai.use(chaiHttp);
 
+// Helper function to get auth token
+const getAuthToken = () => {
+  return jwt.sign(
+    { userId: 'testuser' }, 
+    process.env.JWT_SECRET, 
+    { expiresIn: '1h' }
+  );
+};
 
+describe('Product API Tests', () => {
+  let authToken;
 
+  before((done) => {
+    // Get auth token before tests run
+    authToken = getAuthToken();
+    done();
+  });
 
-describe('Test', () => {
-
-
-
-
-
-it('should POST a valid product', (done) => {
-        
+  it('should POST a valid product', (done) => {
     let product = {
-        name: "Test Product",
-        price: 100,
-        quantity: 20
-    }
+      name: "Test Product",
+      price: 100,
+      quantity: 20
+    };
+    
     chai.request(server)
-    .post('/api/products')
-    .send(product)
-    .end((err, res) => {
+      .post('/api/products')
+      .set('Authorization', `Bearer ${authToken}`) // Add auth header
+      .send(product)
+      .end((err, res) => {
         res.should.have.status(201);
-        
         done();
-
-    });
-
-});
-
+      });
+  });
 });
